042c3b0389ca7d2637d8caa2b66c14320fa59cec
Arun <arun@siara.cc>
2023-05-03 23:21:03 +0530

    Changes to compile script

--- compile_gen_words.sh
+++ compile_gen_words.sh
@@ -23,7 +23,7 @@ if [ ! -f "lid.176.bin" ]; then
 fi
 COMP_OPTS="-O3 -std=c++17"
 # -fsanitize=address"
-g++ ${COMP_OPTS} -L/usr/local/lib -I. -Imisc -I../mimalloc/include -I/usr/local/include -I../fasttext/src -I../bloom/src -I../lmdb/libraries/liblmdb gen_words_and_phrases.cpp sqlite3.o /usr/local/lib/libzstd.a ../fasttext/libfasttext.a ../index_research/src/basix.cpp ../index_research/src/bfos.cpp ../index_research/src/univix_util.cpp ../bloom/src/bloom.c /usr/local/lib/librocksdb.a ../lmdb/libraries/liblmdb/liblmdb.a /usr/local/lib/libwiredtiger.a -lpthread -ldl -llz4 -lbz2 -lzstd -lz -lsnappy -lbrotlienc
+g++ ${COMP_OPTS} -L/usr/local/lib -I. -Imisc -I../mimalloc/include -I/usr/local/include -I../fasttext/src -I../bloom/src -I../lmdb/libraries/liblmdb gen_words_and_phrases.cpp sqlite3.o /usr/local/lib/libzstd.a ../fasttext/libfasttext.a ../index_research/src/univix_util.cpp /usr/local/lib/librocksdb.a ../lmdb/libraries/liblmdb/liblmdb.a /usr/local/lib/libwiredtiger.a -lpthread -ldl -llz4 -lbz2 -lzstd -lz -lsnappy -lbrotlienc
 #-fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -ljemalloc
 
 # screen sh -c './a.out RC_2013-04.zst 5000 4096 rocksdb >> logs/aws_m6gd/rocksdb_320mb_22dec.log 2>>fail.log '
